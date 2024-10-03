f92d23ebe793b7e77d39e77fd7c7c3214754f1f4
divmish-1007 <divakarmishra5301@gmail.com>
2024-10-03 20:19:09 +0530

    Add folder contents to existing repository

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,14 @@
+
+import Chai from './Chai'
+
+function App() {
+  return (
+    <>
+    <Chai/>
+    <h2>Jai ho</h2>
+    </>
+    
+  );
+}
+
+export default App;
---
+++ 01viteReact/src/App.jsx
@@ -0,0 +1,15 @@
+import Chai from "./chai"
+
+function App() {
+
+
+  return (
+  <>
+  <Chai/>
+  <h1>Wrapped in a enclosing tag</h1>
+  <p>it's Needed to wite multiple adjacent JSX</p>
+  </>
+  )
+}
+
+export default App
