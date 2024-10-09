bed9a9f8d865fd2ede9654ad1695ea55b8758232
divmish-1007 <divakarmishra5301@gmail.com>
2024-10-09 11:08:50 +0530

    Lecture No. 4 JSX into React,createElement

--- 01viteReact/src/App.jsx
+++ 01viteReact/src/App.jsx
@@ -2,10 +2,12 @@ import Chai from "./chai"
 
 function App() {
 
+  const username = "chai aur code"
 
   return (
   <>
   <Chai/>
+  <p>This variable username is written as {username}</p>
   <h1>Wrapped in a enclosing tag</h1>
   <p>it's Needed to wite multiple adjacent JSX</p>
   </>
--- 01viteReact/src/main.jsx
+++ 01viteReact/src/main.jsx
@@ -1,10 +1,46 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'; // Refering ful React Line 1 relaced by this line 
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Chai from "./chai"
 
+function MyApp(){
+  return (
+    <div>
+      <h1>Custom APP ! || chai</h1>
+    </div>
+  )
+}
+
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https:/google.com',
+//       target: '_blanke'
+//   }, 
+//   children: 'Click me to visit google'
+// }
+
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
+const reactElement = React.createElement(
+  'a',
+  {href:"https://google.com", target: '_blank'},
+  'click on me to Visit Google by YourSelf'
+)
 
 createRoot(document.getElementById('root')).render(
+
   <StrictMode>
-    <App />
+    <>
+    <App/>
+    </>
+
   </StrictMode>,
+
+  // reactElement
+
 )
