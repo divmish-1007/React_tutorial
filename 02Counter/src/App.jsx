b512da3fd602e9c36036c6691a3496994e8310af
divmish-1007 <divakarmishra5301@gmail.com>
2024-10-09 19:22:31 +0530

    05 Use of Hooks By creating a Project Counter

---
+++ 02Counter/src/App.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(10)
+
+  // let Counter = 15
+
+  // const addValue = () =>{
+  //   if(counter < 20)
+  //     setCounter(counter+1)
+  // }
+
+  const addValue = () =>{
+
+    setCounter(prevCount=>{
+      counter = prevCount + 1;
+
+      if(counter > 20 ){
+        return 20;
+      }
+      return counter;
+    });
+  }
+
+  const subValue = () =>{
+   if(counter>0)
+    setCounter(counter-1)
+  }
+
+  // const subValue = () =>{
+
+  //   setCounter(counter=>{
+  //     counter = counter - 1;
+
+  //     if(counter < 0 ){
+  //       return 0;
+  //     }
+  //     return counter;
+  //   });
+  // }
+
+
+  return (
+    <>
+      <h1>Chai aur React</h1>
+      <h2>Counter Value: {counter}</h2>
+      <button
+      onClick={addValue}
+      >Add Value {counter}</button>
+      <br/>
+      <button
+      onClick={subValue}
+      >Subtract Value {counter}</button>
+    </>
+  )
+}
+
+export default App
