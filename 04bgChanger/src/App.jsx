d61a910aa1b2cef6e95df35fbac8b77341309db5
divmish-1007 <divakarmishra5301@gmail.com>
2024-10-18 23:08:17 +0530

    Better and efficient way added

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -64,3 +64,53 @@ function App() {
 }
 
 export default App
+
+// The code written below has more readability and flexibility
+
+// import { useState } from "react";
+
+// function App() {
+//   const [color, setColor] = useState("olive");
+
+//   // Array of colors for the buttons
+//   const colors = [
+//     { name: "Red", value: "red" },
+//     { name: "Green", value: "green" },
+//     { name: "Yellow", value: "yellow" },
+//     { name: "Blue", value: "blue" },
+//     { name: "Orange", value: "orange" },
+//     { name: "Skyblue", value: "skyblue" },
+//     { name: "Black", value: "black" },
+//     { name: "Pink", value: "pink" },
+//     { name: "Lavender", value: "lavender" },
+//     { name: "White", value: "white" },
+//   ];
+
+//   return (
+//     <>
+//       <div
+//         className="w-full h-screen duration-200"
+//         style={{ backgroundColor: color }}
+//       >
+//         <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2 ">
+//           <div className="flex flex-wrap gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+//             {/* Loop through the colors array and create buttons dynamically */}
+//             {colors.map((col) => (
+//               <button
+//                 key={col.value}
+//                 onClick={() => setColor(col.value)}
+//                 className="outline-none px-4 py-1 rounded-full text-balance shadow-lg"
+//                 style={{ backgroundColor: col.value }}
+//               >
+//                 {col.name}
+//               </button>
+//             ))}
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default App;
+
