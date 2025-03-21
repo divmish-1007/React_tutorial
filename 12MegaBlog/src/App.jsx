96c40c24140a1a1173df12fde718daf5a0af4fa2
divmish-1007 <divakarmishra5301@gmail.com>
2025-03-21 22:08:17 +0530

    Mega Project

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,44 @@
+import {useDispatch} from 'react-redux'
+import { useState, useEffect } from 'react'
+import './App.css'
+import authService from "./appwrite/auth"
+import {login, logout} from "./store/authSlice"
+import Header from "./components/header/Header"
+import Footer from './components/footer/footer'
+
+
+
+function App() {
+
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+  
+
+  
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+         TODO:  {/* Outlet */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : (null)
+}
+
+export default App
